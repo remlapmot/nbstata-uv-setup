1cd204eeedc0820b896ff36037bdd700187f1bae
Tom Palmer <remlapmot@hotmail.com>
2025-04-29 10:07:25 +0100

    Create setup-macos.sh

---
+++ setup-macos.sh
@@ -0,0 +1,22 @@
+# Required so nbstata can find pystata package
+export PYTHONPATH=/Applications/Stata/utilities
+
+# Setup virtual environment
+uv venv --python 3.13
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Install Python dependency packages for nbstata
+uv pip install jupyterlab nbstata jupyterlab_stata_highlight2
+# Additional Python packages if using parameterised Quarto documents
+# uv pip install papermill python-dotenv jupyter-cache
+
+# Setup the nbstata Jupyter kernel
+python -m nbstata.install
+
+# Do work
+# quarto render index.qmd --execute --execute-daemon-restart
+
+# Deactivate virtual environment
+deactivate
