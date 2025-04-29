b7f5cbfeb261ae0a7071b2224b88eb285b2ca59a
Tom Palmer <remlapmot@hotmail.com>
2025-04-29 10:07:30 +0100

    Create setup-windows.sh

---
+++ setup-windows.sh
@@ -0,0 +1,22 @@
+# Required so nbstata can find pystata package
+export PYTHONPATH="C:/Program Files/Stata18/utilities"
+
+# Setup virtual environment
+uv venv --python 3.13
+
+# Activate the virtual environment
+source .venv/Scripts/activate
+
+# Install Python dependency packages for nbstata
+uv pip install jupyterlab nbstata jupyterlab_stata_highlight2
+# Additional Python packages if using parameterised Quarto documents
+# uv pip install papermill python-dotenv jupyter-cache
+
+# Setup the nbstata Jupyter kernel
+python -m nbstata.install
+
+# Do work
+# quarto render index.qmd --execute --execute-daemon-restart
+
+# Deactivate virtual environment
+deactivate
